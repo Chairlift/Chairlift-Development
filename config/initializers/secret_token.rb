0959e9e2e3605128a8daef163d92359c2d20a4ad
Bent Cardan <bcardan@chairliftdevelopment.com>
2011-10-26 02:57:22 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChairliftDevelopment::Application.config.secret_token = 'b1e65e9ad93a27c658d9579570219b226e7cf2dc1793536d01418357f1f3e4e7a8e84e752d612f92cfc1451d4986086a6a9f6174ad3f08835994f96b847943c0'
